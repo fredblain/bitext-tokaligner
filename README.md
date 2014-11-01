3a661504b004ac64042edef59da195c7e30da75e
feedoo <feedoobox@gmail.com>
2014-11-01 13:00:30 +0100

    add script & license, update README

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 bitext-tokaligner
 =================
 
-A simple algorithm to align (at word level) a source sentence and its reference using a translated version as pivot.
+This script is a simple algorithm to align (at word level) a source sentence and its human-generated reference using a raw translation version as pivot. It was designed to extract new phrase pairs in a real-time post-editing context. Both its usability and its time consuming aspects make 'bitext-tokaligner' a suitable approach for this purpose.
+
+Note that 'bitext-tokaligner' expects a single sentence on its standard input. Hence, if you want using it to align a "bitext", when the reviewing process is simulated for instance, use one line per sentence.
+
+**-- How does it work?**
+
+It's a 3-steps process:
+
+**1.** The bitext-tokaligner needs to know for each word of a raw translation, which word of the source sentence is associated with.
+
+To do so, using the Moses toolkit for the translation step, you must ask the decoder to print out the word-level alignments: `-alignment-output-file [src-to-trans.align]` (see [doc][2] for more details).
+
+> Note that **this step is time-free** since the phrase table (binary or textual) includes word-to-word alignments between source and target phrases. Then Moses can report them in the output.
+
+
+**2.** Compute the edit distance (Levenstein) between a raw translation and a reference, using the TER algorithm (Snover).
+
+To do so, use [TERcpp][3], a c++ implementation of the TER designed by [Christophe Servan][4]:
+
+    $PATH/tercpp-bin --noTxtIds --printAlignments -r [ref] -h [trans]
+
+A file named `[trans].output.alignments` containing the TER informations will be created. Note that the bitext-tokaligner has been tested with TERcpp v0.6.2.
+
+> This step is the most time-consuming part of the process. Nevertheless, TERcpp **requires less than 5 sec** to compute the edit-path over a test set containing 1,5k sentences pairs (about 35k words).
+
+**3.** Finally, use the script 'bitext-tokalign.pl' on the previous alignment informations to deduce a word-level source-to-reference alignment, as follow:
+
+    $PATH/bitext-tokalign.pl [src-to-trans.align] [trans-to-ref.align] > [src-to-ref.align]
+
+> It was measured that 'bitext-tokalign.pl', applied on the same test set as step2, **requires less than 1 sec** for computing the src-to-ref alignment using both src-to-trans and trans-to-ref alignments.
+
+**-- Reference**
+
+If you use the bitext-tokaligner for your work, please cite this article in your publications:
+
+Frederic Blain, Holger Schwenk, Jean Senellart, "Incremental Adaptation Using Translation Information and Post-Editing Analysis", Proceedings of the International Workshop on Spoken Language Translation (IWSLT), Hong-Kong, China, December 2012.
+
+[1]: http://statmt.org/moses/ "Moses toolkit"
+[2]: http://www.statmt.org/moses/?n=Moses.AdvancedFeatures#w2walignment "w2w alignment in Moses"
+[3]: http://tercpp.sourceforge.net/ "TERcpp"
+[4]: http://fr.linkedin.com/pub/christophe-servan/24/752/875 "Christophe Servan"
---
+++ bitext-tokalign.pl
@@ -0,0 +1,117 @@
+#! /usr/bin/perl -w
+#
+# bitext-tokalign.pl
+#
+# Copyright © 2014 Frédéric Blain <frederic.blain@lium.univ-lemans.fr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use utf8;
+use Switch;
+use List::MoreUtils qw(firstidx);
+
+my $srcfile; my $reffile;
+BEGIN {
+  die "ERROR: wrong number of argument ($#ARGV instead of 2)\n
+  Usage: $0 src2tgt tgt2ref > src2ref (STDOUT)\n\n" if ($#ARGV != 1);
+
+  open $srcfile, "cat $ARGV[0] |" or die "ERROR: the source file is not reachable ($ARGV[0])\n";
+  open $reffile, "cat $ARGV[1] | egrep '^ALIG:' |" or die "ERROR: the ref file is not reachable ($ARGV[1])\n";
+
+  my $date=`date`;
+  print STDERR "Alignment script is starting:\t$date";
+}
+
+END {
+  my $date=`date`;
+  print STDERR "The alignment is done at: $date";
+}
+
+#-------------------#
+# Requirements:
+# 1/ Moses word-to-word alignment between SRC & TGT         [src2tgt]
+# 2/ Tercpp alignment computed between TGT & REF            [tgt2ref]
+# Output:
+#  SRC<=>REF alignments using the 'grow-diag-final-and' formatting (i.e: 0-0 1-1 2-2 3-..)  [src2ref]
+
+while (my $src2tgt = <$srcfile>)
+{
+  chomp $src2tgt;
+  my $tgt2ref = <$reffile>; chomp $tgt2ref;
+
+  ## SRC <=> TGT alignments
+  my %hashSRC2TGT = ();
+  foreach my $al (split(' ',$src2tgt))
+  {
+    my ($src,$tgt) = ($al =~ m/(\d+)-(\d+)/);
+    $hashSRC2TGT{$src}{$tgt} = 1;
+  }
+
+  ## TGT <=> REF alignments
+  my (%hashTGT2REF, %hashTemp, @tabTGT, @isShift) = ();
+  my $iref = 0;
+  my $itgt = 0;
+  my ($alignments,$nbShift,$shifts) = ($tgt2ref =~ /^ALIG:\s(.*)\s+\|\|\|\s+NbrShifts:\s(\d+)\s?(.*)$/);
+  my @tabalign = split(' ',$alignments);
+  for (my $i=0; $i <= $#tabalign; $i++){ $tabTGT[$i] = $i; }
+
+  if ($nbShift)
+  {
+    my @tabShift = ($shifts =~ /\[.+?\]/g);
+
+    foreach my $shift (@tabShift)
+    {
+      my ($start,$end,$pos) = ($shift =~ /\[(\d+),\s(\d+),\s-?\d+\/(-?\d+)\]/);
+      for (my $sh_pos = $start; $sh_pos <= $end; $sh_pos++){ $isShift[$sh_pos] = $sh_pos; }
+
+      my $size = 1+($end-$start);
+      my @shift = ();
+      my $idx = firstidx{ $_ == $start } @tabTGT;
+
+      @shift = splice(@tabTGT,$idx,$size);
+      if ( $pos-($end-$start) >= 0 ){ splice(@tabTGT, $pos-($end-$start), 0, @shift); }
+      else { splice(@tabTGT, 0, 0, @shift); }
+    } #for
+  } #if nbshift
+
+  foreach my $edType (@tabalign)
+  {
+    switch ($edType)
+    {
+      case 'A'  { $hashTGT2REF{$tabTGT[$itgt]}{$iref} = 1; $itgt++; $iref++; }
+      case 'S'  { if(! exists $isShift[$itgt]){ $hashTGT2REF{$tabTGT[$itgt]}{$iref} = 1; }
+                  $itgt++; $iref++;
+                }
+      case 'I'  { if($iref > 0){ $hashTGT2REF{$tabTGT[$itgt]}{$iref-1} = 1; }
+                  $itgt++;
+                }
+      case 'D'  { $iref++; }
+      else { print "[Error TERCpp alignment] error in editType: $edType\n"; }
+    }
+  } #foreach edType
+
+  ## SRC <=> REF alignment
+  my @out = ();
+  foreach my $src (sort keys %hashSRC2TGT) {
+    foreach my $tgt (sort keys %{$hashSRC2TGT{$src}}) {
+      foreach my $ref (sort keys %{$hashTGT2REF{$tgt}}) { $out[++$#out] = $src."-".$ref; }
+    }
+  }
+  print STDOUT "@out\n";
+} #while
+
+close $srcfile;
+close $reffile;
